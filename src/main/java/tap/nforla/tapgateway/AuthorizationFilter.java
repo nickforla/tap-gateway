da89424dc2fdc3dee8757b4ac864d203a80cfd3b
nforla <nicoforlano@protonmail.com>
2019-11-10 14:56:29 -0300

    Filtro de Autorización JWT

---
+++ src/main/java/tap/nforla/tapgateway/AuthorizationFilter.java
@@ -0,0 +1,87 @@
+package tap.nforla.tapgateway;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class AuthorizationFilter extends ZuulFilter {
+
+    private final Logger logger = LoggerFactory.getLogger(AuthorizationFilter.class);
+    @Value("${GATEWAY_JWT_SECRET}")
+    private String jwtSharedSecret;
+    private final String TOKEN_HEADER = "Authorization";
+    private final String TOKEN_PREFIX = "Bearer ";
+
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+
+        if(!request.getRequestURI().equals("/auth")){
+
+            String token = request.getHeader(TOKEN_HEADER);
+
+            try{
+
+                if(StringUtils.isNotEmpty(token) && token.startsWith(TOKEN_PREFIX)){
+
+                    byte[] signingKey = jwtSharedSecret.getBytes();
+
+                    Jws<Claims> claimsJws =  Jwts.parser()
+                            .setSigningKey(signingKey)
+                            .parseClaimsJws(token.replace(TOKEN_PREFIX, ""));
+
+                    String username = claimsJws.getBody().getSubject();
+
+                    logger.info(username);
+
+                    if(StringUtils.isNotEmpty(username)){
+                        logger.info(String.format("Token válido: %s autorizado", username));
+                        return null;
+                    }
+                }
+
+                throw new IllegalArgumentException();
+
+            }catch (JwtException | IllegalArgumentException exc){
+
+                logger.warn("JWT no válido");
+                context.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+                context.setSendZuulResponse(false);
+                context.setResponseBody("{\"mensaje\": \"JWT no válido\"}");
+
+            }
+        }
+
+        return null;
+    }
+}
--- src/main/java/tap/nforla/tapgateway/TapGatewayApplication.java
+++ src/main/java/tap/nforla/tapgateway/TapGatewayApplication.java
@@ -3,6 +3,7 @@ package tap.nforla.tapgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -12,4 +13,8 @@ public class TapGatewayApplication {
 		SpringApplication.run(TapGatewayApplication.class, args);
 	}
 
+	@Bean
+	public AuthorizationFilter authorizationFilter(){
+		return new AuthorizationFilter();
+	}
 }
