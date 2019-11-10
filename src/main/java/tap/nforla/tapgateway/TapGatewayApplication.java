d8fda20bd479079da512f83082a968998cb48896
nforla <nicoforlano@protonmail.com>
2019-11-10 11:44:28 -0300

    Commit inicial

---
+++ src/main/java/tap/nforla/tapgateway/TapGatewayApplication.java
@@ -0,0 +1,13 @@
+package tap.nforla.tapgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tap/nforla/tapgateway/TapGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package tap.nforla.tapgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
